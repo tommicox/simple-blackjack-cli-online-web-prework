272f99012f90dc272e09fecef0e06a554e57285e
Tommi Cox <tommicox@gmail.com>
2019-03-03 04:46:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,7 +40,8 @@ end
 
 def invalid_command
   # code invalid_command here
-  puts "Please enter a valid command #{prompt_user}"
+  puts "Please enter a valid command"
+  pr
 end
 
 #####################################################
