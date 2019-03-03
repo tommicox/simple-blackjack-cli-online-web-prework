eda008346fa9c8b8baf66b73fb71f8d0896b5f86
Tommi Cox <tommicox@gmail.com>
2019-03-03 04:40:50 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ end
 
 def initial_round
   # code #initial_round here
-  first round = deal_card + deal_card
+ puts  first round = deal_card + deal_card
   display_card_total = (first_round)
   return first_round
   
