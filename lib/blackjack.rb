7209598108ca870d809a6c3658dbec565f97f38b
Tommi Cox <tommicox@gmail.com>
2019-03-03 04:55:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -43,6 +43,13 @@ def hit?(card_total)
   prompt_user
   input = get_user_input
 end
+if input == 'h'
+  card_total += deal_card
+  elsif input == 's'
+  card_total
+end
+end
+  
 
 def invalid_command
   # code invalid_command here
